ef36eadb781f73eecff7bb0f3f1c590e759bf0de
bnguyen22 <43002672+bnguyen22@users.noreply.github.com>
2018-09-26 21:46:44 -0400

    Add files via upload

---
+++ VariableTest.java
@@ -0,0 +1,21 @@
+
+public class VariableTest {
+
+	
+	public static void main(String[] args) {
+		
+
+  
+	char firstInital;    	char lastInital;    	int numLetters;
+
+	firstInital = 'B';  	lastInital ='N';       numLetters = 3;
+
+
+	
+
+		System.out.println("My first initial is " + firstInital + " my last inital is " +  lastInital + " and my name has " + numLetters + " letters.");
+		
+		
+	}
+
+}
